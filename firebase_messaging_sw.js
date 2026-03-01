479fd7af331f8186b7d9c7c6df861077f6285107
dnplus-py <dnchatsmessenger@gmail.com>
2026-03-01 14:53:23 -0300

    Update firebase_messaging_sw.js

--- firebase_messaging_sw.js
+++ firebase_messaging_sw.js
@@ -1,7 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
 
-// Configuración de tu proyecto DNPlus
 firebase.initializeApp({
     apiKey: "AIzaSyD2nZF5QC-Zti80xP1A518qbUPnhRru_9A",
     projectId: "dnplus-messenger-pro",
@@ -10,13 +9,20 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// Recibir notificaciones en segundo plano
+// Escuchar mensajes en segundo plano
 messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'https://dnplus-py.github.io/DNPlus-Messenger-web/icono.png',
-        badge: 'https://dnplus-py.github.io/DNPlus-Messenger-web/icono.png'
+    console.log('Mensaje recibido en segundo plano: ', payload);
+
+    const titulo = payload.data.title || "Llamada de DNPlus";
+    const opciones = {
+        body: payload.data.body || "Tienes una llamada entrante",
+        icon: payload.data.foto || 'https://dnplus-py.github.io/DNPlus-Messenger-web/icono.png',
+        click_action: payload.data.click_action,
+        vibrate: [200, 100, 200, 100, 200, 100, 400], // Vibración tipo llamada
+        tag: 'llamada-dnplus', // Evita que se amontonen
+        renotify: true,
+        data: payload.data
     };
-    self.registration.showNotification(notificationTitle, notificationOptions);
+
+    self.registration.showNotification(titulo, opciones);
 });
